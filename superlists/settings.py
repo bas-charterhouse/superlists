25343499bcbe327fb8e85f2003c79d617dc22df8
bennycoding <benny.coding@gmail.com>
2019-10-29 15:37:29 +0000

    staging changes

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^+=#t7lbi8whfn&rotehp&xm-@aa*bb8#tu$r9j-&t-c$!v-lz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
