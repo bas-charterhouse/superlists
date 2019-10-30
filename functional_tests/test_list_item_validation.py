037a48ec2348c0ecc8b35e9e972ea80f95291633
bennycoding <benny.coding@gmail.com>
2019-10-30 18:08:57 +0000

    New Form with duplication validation added

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.keys import Keys
 from unittest import skip
+from django.utils.html import escape
 from .base import FunctionalTest
-
+from lists.forms import DUPLICATE_ITEM_ERROR
 
 class ItemValidationTest(FunctionalTest):
+    
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -58,6 +60,6 @@ class ItemValidationTest(FunctionalTest):
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
-            "You've already got this in your list"
+            DUPLICATE_ITEM_ERROR
         ))
          
